a2ed5ccab86de846a61693535c8906a4f16dc53e
Ronald Hecker <ronald@caveo.nl>
2010-08-26 22:17:56 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '277a85513eb571497a6643a97493d86a40f302b42c2c0de276773e3abdb1e3025db4eea106899f7c22809b1cf491377bc8ca3688d7935334390f188a056babca'
---
+++ features/website.feature
@@ -0,0 +1,8 @@
+Feature: Show website pages
+	In order to make l2code awesomeness known to people
+	As l2code
+	I want to see pages and its content
+
+	Scenario: See homepage
+		Given I am on the homepage
+		Then I should see "L2Code"
